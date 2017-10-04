84bc18228a45c815db73bc238a68bce79043fe04
erkan <erkan.civelek@msn.com>
2017-10-04 12:23:33 +0300

    yakıt hesaplayıcı

---
+++ Ödevler/yakitHesaplayici.py
@@ -0,0 +1,6 @@
+lira = int(input("Araca kaç lira benzin attın?: "))
+km = int(input("Attığın benzinle kaç km gittin?: "))
+
+sonuc = lira/km
+
+print("Aracınız kilometrede {} kuruş yakmıştır.".format(float(sonuc)))
