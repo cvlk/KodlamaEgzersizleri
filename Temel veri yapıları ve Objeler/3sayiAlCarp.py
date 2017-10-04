26e6910a4f4e14c01cd5f2ca4e570774433c25bb
erkan <erkan.civelek@msn.com>
2017-10-04 12:18:33 +0300

    boykiloindeksi

---
+++ Temel veri yapıları ve Objeler/3sayiAlCarp.py
@@ -0,0 +1,7 @@
+sayi1= int(input("Çarpmak istediğiniz 1. sayıyı giriniz: "))
+sayi2= int(input("Çarpmak istediğiniz 2. sayıyı giriniz: "))
+sayi3= int(input("Çarpmak istediğiniz 3. sayıyı giriniz: "))
+
+sonuc= sayi1*sayi2*sayi3
+
+print("Girdğiniz 3 sayının çarpımı: {}".format(sonuc))
---
+++ Temel veri yapıları ve Objeler/boyKiloindeksi.py
@@ -0,0 +1,8 @@
+boy= float(input("Boyunuzu giriniz: "))
+kilo= float(input("Kilonuzu giriniz: "))
+
+"""Beden Kitle İndeksi : Kilo / Boy(m) Boy(m)"""
+
+index= kilo/(boy*boy)
+
+print("Beden Kitle İndeksiniz: {}".format(index))
